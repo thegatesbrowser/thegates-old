b7e525fbd2078516627c6e6b6dcb4b7c68bf3311
Nordup <nordup.ondr@gmail.com>
2023-05-19 04:09:23 +0300

    input syncronization

--- modules/zmq/input_sync.cpp
+++ modules/zmq/input_sync.cpp
@@ -1,33 +1,58 @@
 
 #include "input_sync.h"
 #include <socket.hpp>
+#include "core/input/input.h"
+#include "core/core_string_names.h"
+#include "core/variant/variant.h"
+#include "core/variant/variant_parser.h"
+
+static inline String var_to_str(const Variant &p_var) {
+	String vars;
+	VariantWriter::write_to_string(p_var, vars);
+	return vars;
+}
+
+static inline Variant str_to_var(const String &p_var) {
+	VariantParser::StreamString ss;
+	ss.s = p_var;
+
+	String errs;
+	int line;
+	Variant ret;
+	(void)VariantParser::parse(&ss, ret, errs, line);
+
+	return ret;
+}
 
 void InputSync::_bind_methods() {
-    ClassDB::bind_method(D_METHOD("bind", "address"), &InputSync::bind, DEFVAL(IPC_ADDR));
+	ClassDB::bind_method(D_METHOD("bind", "address"), &InputSync::bind, DEFVAL(IPC_ADDR));
+	ClassDB::bind_method(D_METHOD("send_input_event", "event"), &InputSync::send_input_event);
 }
 
 void InputSync::bind(const String &address) {
 	sock.bind(address.utf8().get_data());
-    sock.send("Hello from server!");
-
-    std::string msg;
-    sock.receive(msg);
-    print_line("Received: " + String(msg.c_str()));
 }
 
 void InputSync::connect(const String &address) {
-    sock.connect(address.utf8().get_data());
-    sock.send("Hello from client!");
+	sock.connect(address.utf8().get_data());
+}
 
-    std::string msg;
-    sock.receive(msg);
-    print_line("Received: " + String(msg.c_str()));
+void InputSync::send_input_event(const Ref<InputEvent> &event) {
+	sock.send(var_to_str(event).utf8().get_data(), true);
+}
+
+void InputSync::receive_input_events() {
+	std::string msg;
+
+	while (sock.receive(msg, true)) {
+		Input::get_singleton()->parse_input_event((Ref<InputEvent>)str_to_var(msg.c_str()));
+	}
 }
 
 InputSync::InputSync()
-    : sock((zmqpp::socket&&)zmqpp::socket(ctx, zmqpp::socket_type::pair)) {
+	: sock(zmqpp::socket(ctx, zmqpp::socket_type::pair)) {
 }
 
 InputSync::~InputSync() {
-    sock.close();
+	sock.close();
 }
--- modules/zmq/input_sync.h
+++ modules/zmq/input_sync.h
@@ -2,6 +2,7 @@
 #define INPUT_SYNC_H
 
 #include "core/object/ref_counted.h"
+#include "core/input/input.h"
 #include "thirdparty/zmqpp/zmqpp.hpp"
 #include "thirdparty/zmqpp/context.hpp"
 
@@ -9,19 +10,21 @@ inline zmqpp::context ctx;
 static const String IPC_ADDR("ipc://tg-main");
 
 class InputSync : public RefCounted {
-    GDCLASS(InputSync, RefCounted);
+	GDCLASS(InputSync, RefCounted);
 
-    zmqpp::socket sock;
+	zmqpp::socket sock;
 
 protected:
-    static void _bind_methods();
+	static void _bind_methods();
 
 public:
-    void bind(const String &addr = IPC_ADDR);
-    void connect(const String &addr = IPC_ADDR);
+	void bind(const String &addr = IPC_ADDR);
+	void connect(const String &addr = IPC_ADDR);
+	void send_input_event(const Ref<InputEvent> &event);
+	void receive_input_events();
 
-    InputSync();
-    ~InputSync();
+	InputSync();
+	~InputSync();
 };
 
 #endif // INPUT_SYNC_H
