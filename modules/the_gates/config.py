1fe8efb37817be3aabd1c4a78116534f7490ed39
Nordup <nordup.ondr@gmail.com>
2023-05-27 20:04:23 +0300

    build comments

--- modules/the_gates/config.py
+++ modules/the_gates/config.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 def can_build(env, platform):
-    if not env["arch"]: return True
+    if not env["arch"] or env.msvc: return True
     
     if env["platform"] == "windows":
         pkgconf_error = os.system("pkg-config --version > NUL")
@@ -20,10 +20,12 @@ def configure(env):
     if not env["arch"]: return
 
     if env.msvc:
+        # Build libzmq https://www.youtube.com/watch?v=OiGf9T_TPa8
+        # Fix linking mismatch https://stackoverflow.com/questions/28887001/lnk2038-mismatch-detected-for-runtimelibrary-value-mt-staticrelease-doesn
         env.Prepend(CPPDEFINES=["ZMQ_STATIC"])
         env.Prepend(CPPPATH=["C:/Program Files (x86)/ZeroMQ/include"])
-        env.Append(LINKFLAGS=["libzmq-v143-mt-s-4_3_5.lib"])
         env.Append(LIBPATH=["C:/Program Files (x86)/ZeroMQ/lib"])
+        env.Append(LINKFLAGS=["libzmq-v143-mt-s-4_3_5.lib"])
     else:
         if os.system("pkg-config --exists libzmq"):
             print("Error: ZeroMQ librarie not found. Aborting.")
