a2c27b4940c52cca32175b6b4e3d0a3dfecdf1b7
Nordup <nordup.ondr@gmail.com>
2023-05-27 19:55:03 +0300

    clean up win build

--- modules/the_gates/config.py
+++ modules/the_gates/config.py
@@ -20,15 +20,10 @@ def configure(env):
     if not env["arch"]: return
 
     if env.msvc:
-        env.Prepend(CPPPATH=["C:/Program Files (x86)/ZeroMQ/include"])
         env.Prepend(CPPDEFINES=["ZMQ_STATIC"])
+        env.Prepend(CPPPATH=["C:/Program Files (x86)/ZeroMQ/include"])
         env.Append(LINKFLAGS=["libzmq-v143-mt-s-4_3_5.lib"])
-        # env.Append(LINKFLAGS=["libzmq-v143-mt-sgd-4_3_5.lib"])
         env.Append(LIBPATH=["C:/Program Files (x86)/ZeroMQ/lib"])
-        env.Append(LINKFLAGS=["ws2_32.lib", "IPHLPAPI.lib"])
-        env.Append(LIBPATH=["C:/Program Files (x86)/Windows Kits/10/Lib/10.0.22621.0/um/x64"])
-        
-        # env.ParseConfig("pkg-config libzmq --cflags --libs")
     else:
         if os.system("pkg-config --exists libzmq"):
             print("Error: ZeroMQ librarie not found. Aborting.")
