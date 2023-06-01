71ed05c9c3a6ccb167ff621ff0208a7578bb1063
Nordup <nordup.ondr@gmail.com>
2023-06-02 01:54:01 +0300

    send filehandle path

--- modules/the_gates/external_texture.h
+++ modules/the_gates/external_texture.h
@@ -5,13 +5,19 @@
 #include "servers/rendering/rendering_device.h"
 #include "servers/rendering/rendering_device_binds.h"
 
+#ifdef _WIN32
+static const String FILEHANDLE_PATH("ipc://external_texture");
+#else
+static const String FILEHANDLE_PATH("/tmp/external_texture");
+#endif
+
 class ExternalTexture : public RefCounted {
 	GDCLASS(ExternalTexture, RefCounted);
 
-    FileHandle filehandle = FileHandleInvalid;
     RID rid;
     RD::TextureView view;
     RD::TextureFormat format;
+    FileHandle filehandle = FileHandleInvalid;
 
     Error _copy(RID p_texture, bool p_from);
 
@@ -23,8 +29,8 @@ protected:
 public:
     Error create(const RD::TextureFormat &p_format, const RD::TextureView &p_view, const Vector<Vector<uint8_t>> &p_data = Vector<Vector<uint8_t>>());
 	Error import(const RD::TextureFormat &p_format, const RD::TextureView &p_view);
-	bool send_filehandle(const String &p_path);
-	bool recv_filehandle(const String &p_path);
+	bool send_filehandle(const String &p_path = FILEHANDLE_PATH);
+	bool recv_filehandle(const String &p_path = FILEHANDLE_PATH);
 
     Error copy_to(RID p_texture) { return _copy(p_texture, false); };
     Error copy_from(RID p_texture) { return _copy(p_texture, true); };
